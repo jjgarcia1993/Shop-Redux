85f7df0e9b66bb0bc20c5f0d885a16bd2bb7827e
Jonathan Garcia <102456485+jjgarcia1993@users.noreply.github.com>
2022-10-09 16:56:02 -0400

    product

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,13 +1,12 @@
+
 import React from "react";
 import { Link } from "react-router-dom";
-import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
-import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { pluralize } from "../../utils/helpers";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 import { idbPromise } from "../../utils/helpers";
+import { useDispatch, useSelector } from "react-redux";
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
-
   const {
     image,
     name,
@@ -16,17 +15,19 @@ function ProductItem(item) {
     quantity
   } = item;
 
-  const { cart } = state
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
 
+  const { cart } = state;
   const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: _id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
-      idbPromise('cart', 'put', {
+      idbPromise("cart", "put", {
         ...itemInCart,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
@@ -35,9 +36,12 @@ function ProductItem(item) {
         type: ADD_TO_CART,
         product: { ...item, purchaseQuantity: 1 }
       });
-      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
+      idbPromise("cart", "put", {
+        ...item,
+        purchaseQuantity: 1
+      });
     }
-  }
+  };
 
   return (
     <div className="card px-1 py-1">

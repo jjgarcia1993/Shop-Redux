e385799c1ef6f2e0d31abda2b0363af8676875e6
Jonathan Garcia <102456485+jjgarcia1993@users.noreply.github.com>
2022-10-09 16:59:47 -0400

    changes

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,31 +1,33 @@
 export function pluralize(name, count) {
   if (count === 1) {
-    return name;
+    return name
   }
-  return name + 's';
+  return name + 's'
 }
 
 export function idbPromise(storeName, method, object) {
   return new Promise((resolve, reject) => {
     const request = window.indexedDB.open('shop-shop', 1);
+
     let db, tx, store;
-    request.onupgradeneeded = function(e) {
+
+    request.onupgradeneeded = function (e) {
       const db = request.result;
       db.createObjectStore('products', { keyPath: '_id' });
       db.createObjectStore('categories', { keyPath: '_id' });
       db.createObjectStore('cart', { keyPath: '_id' });
     };
 
-    request.onerror = function(e) {
+    request.onerror = function (e) {
       console.log('There was an error');
     };
 
-    request.onsuccess = function(e) {
+    request.onsuccess = function (e) {
       db = request.result;
       tx = db.transaction(storeName, 'readwrite');
       store = tx.objectStore(storeName);
 
-      db.onerror = function(e) {
+      db.onerror = function (e) {
         console.log('error', e);
       };
 
@@ -36,7 +38,7 @@ export function idbPromise(storeName, method, object) {
           break;
         case 'get':
           const all = store.getAll();
-          all.onsuccess = function() {
+          all.onsuccess = function () {
             resolve(all.result);
           };
           break;
@@ -46,9 +48,9 @@ export function idbPromise(storeName, method, object) {
         default:
           console.log('No valid method');
           break;
-      }
+      };
 
-      tx.oncomplete = function() {
+      tx.oncomplete = function () {
         db.close();
       };
     };
